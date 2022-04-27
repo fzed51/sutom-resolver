3887cf364229f3995a83ba761df54ee504366100
Fabien Sanchez <fzed51+git@gmail.com>
2022-04-27 22:03:40 +0200

    feat / ajout de page pour le SuggestionSelector

--- app/components/suggestion-selector/index.tsx
+++ app/components/suggestion-selector/index.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useMemo, useState } from "react";
+import ButtonSecondary from "../buttons/secondary";
 
 export interface SuggestionSelectorProps {
   suggestion: string[];
@@ -9,13 +10,28 @@ export const SuggestionSelector: FC<SuggestionSelectorProps> = ({
   suggestion,
   onSelect,
 }) => {
+  const lengthPage = 20;
+  const [page, setPage] = useState<number>(0);
+  useEffect(() => {
+    setPage(0);
+  }, [suggestion]);
+  const isFirst: boolean = page === 0;
+  const isLast: boolean = page >= Math.floor(suggestion.length / 20);
+  const next = () => setPage((p) => p + 1);
+  const prev = () => setPage((p) => p - 1);
+  const showSuggest = useMemo(
+    () => suggestion.slice(page * lengthPage, (page + 1) * lengthPage - 1),
+    [suggestion, page, lengthPage]
+  );
   return (
     <div>
-      {suggestion.map((word) => (
+      {!isFirst && <ButtonSecondary onClick={prev}>Prev</ButtonSecondary>}
+      {showSuggest.map((word) => (
         <div key={word} onClick={() => onSelect(word)}>
           {word}
         </div>
       ))}
+      {!isLast && <ButtonSecondary onClick={next}>Next</ButtonSecondary>}
     </div>
   );
 };
