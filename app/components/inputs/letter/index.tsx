f8483e919dc8945ee0a93261c79761c67b5eeb72
Fabien Sanchez <fzed51+git@gmail.com>
2022-05-23 00:48:08 +0200

    feat / amelioration du inputLetter

--- app/components/inputs/letter/index.tsx
+++ app/components/inputs/letter/index.tsx
@@ -1,29 +1,45 @@
-import React, { ChangeEvent, FC, FocusEvent, useMemo } from "react";
+import React, { FC, KeyboardEvent, MouseEvent, useMemo } from "react";
 import { InputProps } from "../common";
+import cn from "classnames";
 import { idGen } from "../IdGen";
 import "../common.scss";
+import "./letter.scss";
 
 export interface LetterProps extends InputProps<string> {}
 
+const layout = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 export const InputLetter: FC<LetterProps> = ({ label, value, onChange }) => {
   const id = useMemo(() => idGen("letter"), []);
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const v = e.target.value;
-    const l = v.toLowerCase().at(-1);
-    const regex = /[a-z]/;
-    if (regex.test(l)) {
-      onChange(l);
+  const handleChange = (l: string) => (e: MouseEvent) => {
+    onChange(l);
+  };
+  const handleKeyPress = (e: KeyboardEvent<HTMLDivElement>) => {
+    console.log(e);
+    if (layout.toUpperCase().split("").includes(e.key.toUpperCase())) {
+      onChange(e.key.toUpperCase());
     }
   };
   return (
     <div className="input-group">
       {label && <label htmlFor={id}>{label}</label>}
-      <input
-        id={id}
-        value={value || "a"}
-        onChange={handleChange}
-        onFocus={(e: FocusEvent<HTMLInputElement>) => e.target.select()}
-      />
+      <div
+        tabIndex={1}
+        onKeyDown={handleKeyPress}
+        className="input-letter_keybord"
+      >
+        {layout.split("").map((l) => (
+          <div
+            key={l}
+            className={cn("input-letter_key", {
+              selected: l === value,
+            })}
+            onClick={handleChange(l)}
+          >
+            {l}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
